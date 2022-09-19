62868555b048909c3d5be0334f5197db7928ecab
danieldificil <daniel.araujo@ideabox-it.com>
2022-09-18 22:49:30 -0300

    dia 4

--- 03-Laços-de-repetição/01-laços/lacos.js
+++ 03-Laços-de-repetição/01-laços/lacos.js
@@ -1,9 +1,13 @@
 const alunos = ['João', 'Juliano', 'Caio', 'Amanda']
+const mediaDeAlunos = [10,7,9,6]
+const listaDeNotasDeAlunos = [alunos, mediaDeAlunos]
 
-const medisaAluno = [10,7,9,6]
-const listaDeNomesEAlunos = [alunos, medisaAluno]
-const listaDeNotasDeAlunos = [alunos, medisaAluno]
-
-const exibeNomeNota = nomeDoAluno => {
-
+const exibeNomeNota = (nomeDoAluno) => {
+    if(listaDeNotasDeAlunos[0].includes(nomeDoAluno)){
+        let indice = listaDeNotasDeAlunos[0].indexOf(nomeDoAluno)
+        return console.log(`A média de ${listaDeNotasDeAlunos[0][indice]} é ${listaDeNotasDeAlunos[1][indice]}`);
+    }else{
+        return console.log("Aluno não encontrado")
+    }
 }
+exibeNomeNota("jon")
